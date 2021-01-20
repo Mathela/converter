18d5d48c11272a8b063f93c752a0a5b6e6eab22a
Nkosinathi Mathela <nkosinathi.mathela@gmail.com>
2021-01-20 15:31:53 +0200

    temp conversion complete

--- src/main/java/com/wonderlabz/iq/converter/ConverterService.java
+++ src/main/java/com/wonderlabz/iq/converter/ConverterService.java
@@ -5,6 +5,9 @@
  */
 package com.wonderlabz.iq.converter;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,6 +15,30 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Nkosinathi Mathela
  */
 @RestController
+@RequestMapping("/conversions")
 public class ConverterService {
     
+    @GetMapping(path = "/ktoc/{temperature}", produces = "application/xml")
+    public Double convertKelvinToCelsius(@PathVariable Double temperature
+    ){
+        Double newTemp=0.00;
+        newTemp = newTemp -273.15;
+        return newTemp;
+    } 
+    
+    @GetMapping(path = "/ctok/{temperature}", produces = "application/xml")
+    public Double convertCelsiusToKelvin(@PathVariable Double temperature
+    ){
+        Double newTemp=0.00;
+        newTemp = newTemp +273.15;
+        return newTemp;
+    } 
+    
+       @GetMapping(path = "/mtok/{distance}", produces = "application/xml")
+    public Double mileToKm(@PathVariable Double distance
+    ){
+        Double newDistance=0.00;
+        newDistance = newDistance * 1.61 ;
+        return newDistance;
+    } 
 }
